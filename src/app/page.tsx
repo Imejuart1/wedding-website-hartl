cf40699395dab5b8d4b3fa7aecf5f3b108f9c2d3
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2024-11-22 20:07:27 +0100

    wedding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
+"use client"
+import { useEffect, useState } from 'react';
 import styles from './Home.module.scss';
 
 export default function Home() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
   return (
     <div className={styles.container}>
       {/* Navigation Bar */}
       <nav className={styles.navbar}>
-        <div className={styles.logo}>Tanseli & Hartl</div>
+        <div className={styles.logo}>Tanseli & Roger</div>
         <div className={styles.navLinks}>
           <a href="#wedding" className={styles.navLink}>The Wedding</a>
           <a href="#registry" className={styles.navLink}>Registry</a>
@@ -17,30 +24,34 @@ export default function Home() {
       {/* Hero Section */}
       <section className={styles.hero}>
         {/* Left Section: Text */}
-        <div className={styles.left}>
-          <h1 className={styles.title}>Tanseli & Hartl</h1>
+        <div className={`${styles.left} ${isLoaded ? styles.animateLeft : ''}`}>
+          <h1 className={styles.title}>Tanseli & Roger</h1>
           <p className={styles.subtitle}>We're getting married!</p>
-          <p className={styles.date}>Saturday, April 22, 2025</p>
+          <p className={styles.date}>Saturday, August 22, 2024</p>
           <a href="#rsvp" className={styles.rsvpButton}>RSVP</a>
         </div>
 
         {/* Right Section: Background Image */}
-        <div className={styles.right}>
+        <div className={`${styles.right} ${isLoaded ? styles.animateRight : ''}`}>
           <img
-            src="/img/weddinghall.jpg"
+            src="/img/mary.png"
             alt="Background trees"
             className={styles.backgroundImage}
           />
         </div>
 
         {/* Floating Image of the Couple */}
-        <div className={styles.floatingImage}>
+       {/*} <div className={styles.floatingImage}>
           <img
-            src="/img/tanseli.jpeg"
-            alt="Tanseli and Hartl"
+            src="/img/Rogertanseli.jpg"
+            alt="Tanseli and Roger"
             className={styles.coupleImage}
           />
-        </div>
+        </div>*/}
+      </section>
+
+      <section className={styles.excite}>
+        <p>We're so excited to celebrate this special moment with our closest loved ones!</p>
       </section>
     </div>
   );
